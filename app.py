3b57346aac6f44fc62a2f060fce56d0dbfe96d94
Eric van Zanten <eric.vanzanten@gmail.com>
2015-09-11 08:26:22 -0500

    Aborting when requested image doesn't exist instead of saving placeholder

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, abort
 import json
 import requests
 from boto.s3.connection import S3Connection
@@ -17,21 +17,23 @@ def index(pin):
     s3_key = Key(bucket)
     s3_key.key = '{0}.jpg'.format(pin)
 
-    try:
+    if s3_key.exists():
         output = BytesIO()
         s3_key.get_contents_to_file(output)
-    
-    except S3ResponseError:
-        
+
+    else:
         image_viewer = 'http://cookviewer1.cookcountyil.gov/Jsviewer/image_viewer/requestImg.aspx?{0}='
         image_url = image_viewer.format(pin)
         image = requests.get(image_url)
         
-        output = BytesIO(image.content)
-        s3_key.set_metadata('Content-Type', 'image/jpg')
-        s3_key.set_contents_from_file(output)
-        s3_key.set_acl('public-read')
-        
+        if image.headers['Content-Type'] == 'image/jpeg':
+            output = BytesIO(image.content)
+            s3_key.set_metadata('Content-Type', 'image/jpg')
+            s3_key.set_contents_from_file(output)
+            s3_key.set_acl('public-read')
+        else:
+            abort(404)
+    
     output.seek(0)
     response = make_response(output.read())
     response.headers['Content-Type'] = 'image/jpg'
